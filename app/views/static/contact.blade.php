226e9c26784b106d6001545b54d5f089e6a95d84
dexter <dexmaster3@gmail.com>
2016-11-07 20:29:47 +0000

    email->image for spam

--- app/views/static/contact.blade.php
+++ app/views/static/contact.blade.php
@@ -6,7 +6,7 @@
         <div class="col-lg-12">
             <h1>Channels of Communication</h1>
             <p>Feel free to contact me using any of the following methods</p><br/>
-            <h5>Email: dex@dexcaff.com</a></h5>
+            <h5>Email: {{ HTML::image('img/dexemail.png') }}</a></h5>
             <h5>Telephone: {{ HTML::image('img/phone.png') }}</h5>
             <h5>Postcard: 40 Percy Street, Charleston, SC 29403</h5>
             <h5>Fax Machine: 1-823-352-7647</h5>
